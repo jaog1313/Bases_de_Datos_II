80400304ce1594f899f8908f2a1d2246a62307e0
jaog1313 <36735007+jaog1313@users.noreply.github.com>
2021-11-18 08:19:14 -0500

    Desarrollando punto 01. Un error, las tablas eran las departamentos y empleados de la practica 01

--- Lab_Bases_de_Datos/Practica_04/Ortiz_Jorge_practica_04.sql
+++ Lab_Bases_de_Datos/Practica_04/Ortiz_Jorge_practica_04.sql
@@ -1,20 +1,20 @@
 /
 -- Funcion 01
-CREATE OR REPLACE FUNCTION get_deptno_by_name(
-	p_dname dept.dmane%TYPE
+CREATE OR REPLACE FUNCTION get_deptid_by_name(
+	p_dnombre departamentos.dnombre%TYPE
 )
 RETURN NUMBER
 IS
-	v_deptno := 0;
+	v_deptid := 0;
 BEGIN
 	SELECT dept.deptno
-	INTO v_deptno
-	FROM dept
-	WHERE LOWER(dept.dname) = LOWER(p_dname);
-	RETURN v_deptno;
+	INTO v_deptid
+	FROM departamentos
+	WHERE LOWER(departamentos.dnombre) = LOWER(p_dnombre);
+	RETURN v_deptid;
 EXCEPTION
 	WHEN NO_DATA_FOUND THEN
-		DBMS_OUTPUT.PUT_LINE('No se encontro el departamento con el nombre' || p_dname);
+		DBMS_OUTPUT.PUT_LINE('No se encontro el departamento con el nombre' || p_dnombre);
 		RETURN -1;
 	WHEN OTHERS THEN
 		DBMS_OUTPUT.PUT_LINE( SQLERRM );
