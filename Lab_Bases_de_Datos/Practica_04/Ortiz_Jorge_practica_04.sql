79df6b5d9863482f7a8767ef0f9415072079e6f3
jaog1313 <36735007+jaog1313@users.noreply.github.com>
2021-11-18 09:05:48 -0500

    Punto 02 terminado. Falta ensayar. Se entrego así la practica

--- Lab_Bases_de_Datos/Practica_04/Ortiz_Jorge_practica_04.sql
+++ Lab_Bases_de_Datos/Practica_04/Ortiz_Jorge_practica_04.sql
@@ -1,3 +1,4 @@
+END;
 /
 -- Funcion 01
 CREATE OR REPLACE FUNCTION get_deptid_by_name(
@@ -38,7 +39,7 @@ EXCEPTION
 	WHEN OTHERS THEN
 		DBMS_OUTPUT.PUT_LINE( SQLERRM );
 END;
-
+/
 -- Segundo llamado de la funcion get_deptid_by_name,  pasandole como parametro un dato no valido.
 -- Debería retornar -1
 
@@ -57,5 +58,45 @@ EXCEPTION
 	WHEN OTHERS THEN
 		DBMS_OUTPUT.PUT_LINE( SQLERRM );
 END;
+/
 
+-- Punto 02
 
+CREATE OR REPLACE FUNCTION get_count_emp_by_name(
+	p_dnombre  departamentos.dnombre%TYPE
+)
+RETURN NUMBER
+IS
+	v_id NUMBER := 0;
+	v_cont number := 0;
+BEGIN
+	v_id = get_deptid_by_name(p_dnombre);
+	SELECT COUNT(*)
+	into v_cont
+	from empleados
+	where deptid = v_id;
+	RETURN v_cont;
+EXCEPTION
+	WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE( SQLERRM );
+		RETURN -1;
+END;
+/
+
+-- Primer llamado de la funcion get_count_emp_by_name, pasandole como parametro un dato valido debería retornar el numero de empleados
+DECLARE
+	v_text VARCHAR2 := 'CONTABILIDAD';
+	v_cont NUMBER := 0;
+BEGIN
+	v_cont = get_count_emp_by_name(v_text);
+	IF v_id <> 0 THEN
+	
+		DBMS_OUTPUT.PUT_LINE( 'El departamento  ' || v_text || ' tiene' || v_cont || ' empleados' );
+	ELSE
+		DBMS_OUTPUT.PUT_LINE( 'El  departamento con nombre ' || v_text || ' no existe');
+		
+EXCEPTION
+	WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE( SQLERRM );
+END;
+/
